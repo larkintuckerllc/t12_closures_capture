3de567985111d0f13fa3bc1b21d7e98749020fc8
John Tucker <john@larkintuckerllc.com>
2019-11-25 08:51:56 -0500

    done

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,54 @@
   console.log(equalToXF(y)); // true
   console.log(equalToX(y)); // true
 }
+
+{
+  const v1 = [1, 2, 3];
+  // 1 2 3
+  v1.forEach(val => {
+    console.log(`Got: ${val}`);
+  });
+  // 1 2 3
+  v1.forEach(val => {
+    console.log(`Got: ${val}`);
+  });
+}
+
+{
+  const v1 = [1, 2, 3];
+  const v2 = v1.map(x => x + 1); // FUNC EXECUTED
+  // 2 3 4
+  v2.forEach(val => {
+    console.log(`Got: ${val}`);
+  });
+}
+
+interface Shoe {
+  size: number;
+  style: string;
+}
+
+const shoesInMySize = (shoes: Shoe[], shoeSize: number): Shoe[] =>
+  shoes.filter(shoe => shoe.size === shoeSize);
+
+{
+  const shoes: Shoe[] = [
+    {
+      size: 10,
+      style: 'sneaker',
+    },
+    {
+      size: 13,
+      style: 'sandal',
+    },
+    {
+      size: 10,
+      style: 'boot',
+    },
+  ];
+  const inMySize = shoesInMySize(shoes, 10);
+  inMySize.forEach(shoe => {
+    // sneaker boot
+    console.log(`Got: ${shoe.style}`);
+  });
+}
